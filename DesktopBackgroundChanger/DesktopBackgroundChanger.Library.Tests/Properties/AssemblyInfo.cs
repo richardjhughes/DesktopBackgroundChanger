777abf68008dbcb60b540826c2cf76a575b59b3d
RH <richard.j.hughes@hotmail.com>
2018-08-04 15:36:44 +0800

    Added base projects

---
+++ DesktopBackgroundChanger/DesktopBackgroundChanger.Library.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DesktopBackgroundChanger.Library.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DesktopBackgroundChanger.Library.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("68cca7ca-c910-4028-95be-065301b1152c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- DesktopBackgroundChanger/DesktopBackgroundChanger.UI.Console/Properties/AssemblyInfo.cs
+++ DesktopBackgroundChanger/DesktopBackgroundChanger.UI.Console/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DesktopBackgroundChanger")]
+[assembly: AssemblyTitle("DesktopBackgroundChanger.UI.Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DesktopBackgroundChanger")]
+[assembly: AssemblyProduct("DesktopBackgroundChanger.UI.Console")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d6d24c82-adec-499a-a5ac-c02678824ed3")]
+[assembly: Guid("525bb572-bfb0-4e7c-947e-49a31bf40830")]
 
 // Version information for an assembly consists of the following four values:
 //
